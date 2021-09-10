13f96caf12b215f904e4e14fd87575ab9e34554e
Bruno Riwerson Silva <riwerson@gmail.com>
2021-09-10 20:38:45 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 //import React from 'react'; em desuso a partir da versão 17 do React
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá, mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá, mundo!</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
